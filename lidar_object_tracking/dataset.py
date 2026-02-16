938a2d843fca3c51cbe5bc6da734ce55adaf0051
Daniel Bossaller <168793316+dbossaller@users.noreply.github.com>
2026-02-16 13:16:11 -0600

    Processed and plotted scaled data

--- lidar_object_tracking/dataset.py
+++ lidar_object_tracking/dataset.py
@@ -1,6 +1,10 @@
+import math
+import os
 from pathlib import Path
+import pickle
 
 from loguru import logger
+import pandas as pd
 from tqdm import tqdm
 import typer
 
@@ -12,7 +16,7 @@ app = typer.Typer()
 @app.command()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
+    input_path: Path = RAW_DATA_DIR,
     output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
     # ----------------------------------------------
 ):
@@ -23,6 +27,29 @@ def main(
             logger.info("Something happened for iteration 5.")
     logger.success("Processing dataset complete.")
     # -----------------------------------------
+
+
+def unpack_dataset(input_path, scene_no):
+    frame_dict = {}
+    for dir in os.scandir(input_path + f"{scene_no}/lidar/"):
+        with open(dir, "rb") as file:
+            try:
+                frame_dict[int(dir.name[0:2])] = pd.DataFrame(pickle.load(file))
+            except:
+                pass
+
+    return dict(sorted(frame_dict.items()))
+
+
+def rotate_45(x_val, y_val):
+    angle = -math.pi / 4
+    x_rot = x_val * math.cos(angle) + y_val * math.sin(angle)
+    y_rot = -x_val * math.sin(angle) + y_val * math.cos(angle)
+    return x_rot, y_rot
+
+
+def df_to_json(dataset, output_path):
+    return dataset.to_json(output_path, indent=2)
 
 
 if __name__ == "__main__":
