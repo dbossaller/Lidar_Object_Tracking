4ef578ff30b23938e417231523d9ed32197caeeb
Daniel Bossaller <168793316+dbossaller@users.noreply.github.com>
2026-02-17 16:14:53 -0600

    continued putting together processing script

--- lidar_object_tracking/dataset.py
+++ lidar_object_tracking/dataset.py
@@ -26,28 +26,14 @@ def main(
             rotate_45(frame_dict[i]["x"], frame_dict[i]["y"])[1],
         )
 
+    coord_by_frame = {}
     for i in frame_dict.keys():
-        frame_dict[i].drop(
-            frame_dict[i][
-                (frame_dict[i]["xr"].apply(abs) > 25)
-                | (frame_dict[i]["yr"] < 0)
-                | (frame_dict[i]["yr"] > 25)
-                | (frame_dict[i]["z"] > 5)
-                | (frame_dict[i]["z"] < 0)
-            ].index,
-            inplace=True,
-        )
+        crop_dataset(frame_dict[i])
 
-    sub_sample = [sample(list(frame_dict[i].index), 1500) for i in range(len(frame_dict))]
+    for i in frame_dict.keys():
+        coord_by_frame[i] = sample_rotated_point_cloud(frame_dict[i])
 
-    coord_by_frame = {
-        i: frame_dict[i][["xr", "yr", "z"]].loc[sub_sample[i]] for i in range(len(frame_dict))
-    }
-
-    data_to_json(coord_by_frame, output_path, "004")
-
-
-def unpack_dataset(input_path, scene_no):
+def unpack_dataset(input_path: Path, scene_no: str):
     frame_dict = {}
     for dir in os.scandir(f"{input_path}/" + f"{scene_no}/lidar/"):
         with open(dir, "rb") as file:
@@ -58,12 +44,30 @@ def unpack_dataset(input_path, scene_no):
 
     return dict(sorted(frame_dict.items()))
 
+def crop_dataset(frame, xmin = -25, xmax = 25, ymin = 0, ymax = 25, zmin=1, zmax=5):
+    filt = (
+    (frame["xr"] < xmin)|
+    (frame["xr"] > xmax)|
+    (frame["yr"] < ymin)|
+    (frame["yr"] > ymax)|
+    (frame["z"] > zmax)|
+    (frame["z"] < zmin))
+    
+    frame.drop(
+        frame[filt
+            ].index,
+            inplace=True,
+        )
 
 def rotate_45(x_val, y_val):
     angle = -math.pi / 4
     x_rot = x_val * math.cos(angle) + y_val * math.sin(angle)
     y_rot = -x_val * math.sin(angle) + y_val * math.cos(angle)
     return x_rot, y_rot
+
+def sample_rotated_point_cloud(frame, num_points = 1500):
+    sample_idxs = sample(list(frame.index), num_points)
+    return frame[['xr','yr','z']].loc[sample_idxs]
 
 
 def data_to_dict(dataset):
@@ -74,7 +78,7 @@ def data_to_dict(dataset):
 
 
 def data_to_json(dataset, output_dir, scene_number):
-    dataset = data_to_dict(dataset=dataset)
+    dataset = data_to_dict(dataset)
     with open(f"{output_dir}" + f"/scene{scene_number}.json", "w+") as f:
         json.dump(dataset, f, indent=2)
 
