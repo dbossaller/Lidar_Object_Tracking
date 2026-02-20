6f5d9ce1bd05052c35a97cc583deaac1e90f099d
Daniel Bossaller <168793316+dbossaller@users.noreply.github.com>
2026-02-20 14:23:12 -0600

    Finished Data Pipeline

--- lidar_object_tracking/dataset.py
+++ lidar_object_tracking/dataset.py
@@ -15,11 +15,9 @@ app = typer.Typer()
 
 @app.command()
 def unpack_to_json(
-    input_path: Path = RAW_DATA_DIR,
-    output_path: Path = PROCESSED_DATA_DIR,
-    scene_number = '004'
+    input_path: Path = RAW_DATA_DIR, output_path: Path = PROCESSED_DATA_DIR, scene_number="004"
 ):
-    frame_dict = unpack_dataset(input_path, scene_number)
+    frame_dict = unpack_from_pickle(input_path, scene_number)
 
     for i in frame_dict.keys():
         frame_dict[i]["xr"], frame_dict[i]["yr"] = (
@@ -33,29 +31,50 @@ def unpack_to_json(
 
     for i in frame_dict.keys():
         coord_by_frame[i] = sample_rotated_point_cloud(frame_dict[i])
-    
-    data_to_json(dataset = coord_by_frame, output_dir=output_path, scene_number='004')
 
-def unpack_dataset(input_path: Path, scene_no: str):
+    data_to_json(dataset=coord_by_frame, output_dir=output_path, scene_number="004")
+
+
+def unpack_from_pickle(input_path: Path, scene_no: str):
     frame_dict = {}
     for dir in os.scandir(f"{input_path}/" + f"{scene_no}/lidar/"):
         with open(dir, "rb") as file:
             try:
                 frame_dict[int(dir.name[0:2])] = pd.DataFrame(pickle.load(file))
-            except:
+            except pickle.UnpicklingError:
                 pass
 
     return dict(sorted(frame_dict.items()))
 
-def crop_dataset(frame, x_span = 25, ymin = 0, ymax = 25, zmin=0, zmax=5):
+
+def pre_process(frame_dict):
+    for i in frame_dict.keys():
+        frame_dict[i]["xr"], frame_dict[i]["yr"] = (
+            rotate_45(frame_dict[i]["x"], frame_dict[i]["y"])[0],
+            rotate_45(frame_dict[i]["x"], frame_dict[i]["y"])[1],
+        )
+
+    coord_by_frame = {}
+    for i in frame_dict.keys():
+        crop_dataset(frame_dict[i])
+
+    for i in frame_dict.keys():
+        coord_by_frame[i] = sample_rotated_point_cloud(frame_dict[i])
+
+    return coord_by_frame
+
+
+def crop_dataset(frame, x_span=25, ymin=0, ymax=25, zmin=0, zmax=5):
     filt = (
-    (frame["xr"].apply(abs) > x_span)|
-    (frame["yr"] < ymin)|
-    (frame["yr"] > ymax)|
-    (frame["z"] > zmax)|
-    (frame["z"] < zmin))
-    
-    frame.drop(frame[filt].index,inplace=True)
+        (frame["xr"].apply(abs) > x_span)
+        | (frame["yr"] < ymin)
+        | (frame["yr"] > ymax)
+        | (frame["z"] > zmax)
+        | (frame["z"] < zmin)
+    )
+
+    frame.drop(frame[filt].index, inplace=True)
+
 
 def rotate_45(x_val, y_val):
     angle = -math.pi / 4
@@ -63,9 +82,10 @@ def rotate_45(x_val, y_val):
     y_rot = -x_val * math.sin(angle) + y_val * math.cos(angle)
     return x_rot, y_rot
 
-def sample_rotated_point_cloud(frame, num_points = 1500):
+
+def sample_rotated_point_cloud(frame, num_points=1500):
     sample_idxs = sample(list(frame.index), num_points)
-    return frame[['xr','yr','z']].loc[sample_idxs]
+    return frame[["xr", "yr", "z"]].loc[sample_idxs]
 
 
 def data_to_dict(dataset):
@@ -76,26 +96,31 @@ def data_to_dict(dataset):
 
 
 def data_to_json(dataset, output_dir, scene_number):
-    dict_data = data_to_dict(dataset = dataset)
+    dict_data = data_to_dict(dataset=dataset)
     with open(f"{output_dir}" + f"/scene{scene_number}.json", "w+") as f:
         json.dump(dict_data, f, indent=2, ensure_ascii=False)
 
+
 def read_data_json(data_directory, scene_no):
-    filename = f'{data_directory}/scene'+f'{scene_no}.json'
+    filename = f"{data_directory}/scene" + f"{scene_no}.json"
     json_data = None
     with open(filename, "r") as dataset:
         json_data = json.load(dataset)
     return json_data
 
+
 def samples_to_dataframes(raw_json):
     df_dataset = {}
     for key in raw_json.keys():
-        df_dataset[int(key)] = pd.DataFrame(raw_json[key],columns = ['xr','yr','z'])
+        df_dataset[int(key)] = pd.DataFrame(raw_json[key], columns=["xr", "yr", "z"])
 
     return df_dataset
+
 
 def load_json_data(data_directory, scene_num):
     json_data = read_data_json(data_directory, scene_num)
     return samples_to_dataframes(json_data)
+
+
 if __name__ == "__main__":
     app()
--- lidar_object_tracking/plots.py
+++ lidar_object_tracking/plots.py
@@ -13,8 +13,8 @@ app = typer.Typer()
 @app.command()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR, 
-    output_path: Path = FIGURES_DIR
+    input_path: Path = PROCESSED_DATA_DIR,
+    output_path: Path = FIGURES_DIR,
     # -----------------------------------------
 ):
     # ---- REPLACE THIS WITH YOUR OWN CODE ----
@@ -25,50 +25,56 @@ def main(
     logger.success("Plot generation complete.")
     # -----------------------------------------
 
+
 def plot_with_slider(dataset):
     fig = go.Figure()
 
     for i in range(80):
         fig.add_trace(
             go.Scatter3d(
-                visible = False,
-                mode = 'markers',
-                marker = dict(size = 2),
-                x = dataset[i]['xr'],
-                y = dataset[i]['yr'],
-                z = dataset[i]['z']
+                visible=False,
+                mode="markers",
+                marker=dict(size=2),
+                x=dataset[i]["xr"],
+                y=dataset[i]["yr"],
+                z=dataset[i]["z"],
             )
         )
-        fig.update_layout(height = 500, width = 750)
-        fig.update_scenes(xaxis =dict(range = [-25,25]), 
-                        yaxis=dict(range = [0,25]), 
-                        zaxis=dict(range = [0,5]),
-                        aspectmode = 'data')
+        fig.update_layout(height=500, width=750)
+        fig.update_scenes(
+            xaxis=dict(range=[-25, 25]),
+            yaxis=dict(range=[0, 25]),
+            zaxis=dict(range=[0, 5]),
+            aspectmode="data",
+        )
 
     fig.data[0].visible = True
 
     steps = []
     for i in range(len(list(fig.data))):
         step = dict(
-            method = 'update',
-            args= [{'visible':[False]*len(list(fig.data))}],
-            label = f'{i/10}s',
+            method="update",
+            args=[{"visible": [False] * len(list(fig.data))}],
+            label=f"{i / 10}s",
         )
-        step['args'][0]['visible'][i] = True
+        step["args"][0]["visible"][i] = True
         steps.append(step)
 
-    sliders = [dict(
-        active = 0,
-        currentvalue = {'prefix': "Frame: ",},
-        pad={'t':80},
-        steps = steps
-    )]
+    sliders = [
+        dict(
+            active=0,
+            currentvalue={
+                "prefix": "Frame: ",
+            },
+            pad={"t": 80},
+            steps=steps,
+        )
+    ]
 
-    fig.update_layout(
-        sliders = sliders
-    )
+    fig.update_layout(sliders=sliders)
 
     fig.show()
-    
+
+
 if __name__ == "__main__":
     app()
